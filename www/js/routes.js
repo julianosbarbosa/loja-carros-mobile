d9547c08ff61716228fc5ead97c9963e0fed5779
Juliano <juliano@nationphone.com>
2017-03-20 18:19:23 -0300

    UPDATE - Iserido rotas de listagem e de carro escolhido

---
+++ www/js/routes.js
@@ -0,0 +1,15 @@
+angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('listagem', {
+            url: '/listagem',
+            templateUrl: 'templates/listagem.html',
+            controller: 'ListagemController'
+        })
+
+    .state('carroescolhido', {
+        url: '/carroescolhido',
+        templateUrl: 'templates/carroescolhido.html',
+        controller: 'CarroEscolhidoController'
+    });
+    $urlRouterProvider.otherwise('listagem');
+});
