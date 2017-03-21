8aad41c14bf78aa138e3df6b92dcc8c70cdec31f
Juliano <juliano@nationphone.com>
2017-03-21 16:05:28 -0300

    UPDATE - formulario finalizando pedido

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,9 +1,21 @@
 angular.module('starter').controller('ListagemController', function($scope) {
 
-    $scope.listaDeCarros = [{ "nome": "BMW 120i", "preco": 70000 }];
-    // '', 'Onix 1.6', 'Fiesta 2.0', 'C3 1.0', 'Uno Fire',
-    // 'Sentra 2.0', 'Astra Sedan', 'Vectra 2.0 Turbo', 'Hilux 4x4', 'Montana Cabine Dupla'
+    $scope.listaDeCarros = [
+        { "nome": "BMW 120i", "preco": 70000 },
+        { "nome": "Onix 1.6", "preco": 35000 },
+        { "nome": "Fiesta 2.0", "preco": 52000 },
+        { "nome": "C3 1.0", "preco": 22000 },
+        { "nome": "Uno Fire", "preco": 11000 },
+        { "nome": "Sentra 2.0", "preco": 53000 },
+        { "nome": "Astra Sedan", "preco": 39000 },
+        { "nome": "Vectra 2.0", "preco": 37000 },
+        { "nome": "Hilux 4x4", "preco": 90000 },
+        { "nome": "Montana Cabine dupla", "preco": 57000 },
+        { "nome": "Outlander 2.4", "preco": 99000 },
+        { "nome": "Fusca 15000", "preco": 6000 }
+    ];
 });
+
 angular.module('starter').controller('CarroEscolhidoController', function($stateParams, $scope) {
     $scope.carroEscolhido = angular.fromJson($stateParams.carro);
     $scope.listaDeAcessorios = [{ "nome": "Freio ABS", "preco": 800 }, { "nome": "Ar Condicionado", "preco": 1000 },
@@ -16,4 +28,8 @@ angular.module('starter').controller('CarroEscolhidoController', function($state
             $scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
         }
     }
+});
+
+angular.module('starter').controller('FinalizarPedidoController', function($stateParams, $scope) {
+    $scope.carroFinalizado = angular.fromJson($stateParams.carro);
 });
--- www/js/routes.js
+++ www/js/routes.js
@@ -7,9 +7,14 @@ angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
         })
 
     .state('carroescolhido', {
-        url: '/carroescolhido/:carro',
-        templateUrl: 'templates/carroescolhido.html',
-        controller: 'CarroEscolhidoController'
-    });
+            url: '/carroescolhido/:carro',
+            templateUrl: 'templates/carroescolhido.html',
+            controller: 'CarroEscolhidoController'
+        })
+        .state('finalizarpedido', {
+            url: '/finalizarpedido/:carro',
+            templateUrl: 'templates/finalizarpedido.html',
+            controller: 'FinalizarPedidoController'
+        });
     $urlRouterProvider.otherwise('listagem');
 });
